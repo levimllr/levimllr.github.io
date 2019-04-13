0261d96fc7367524258d8973d65f7cec24275714
Levi Miller <44423236+levimllr@users.noreply.github.com>
2019-04-13 15:35:58 -0700

    Mobile menu test. Attempt to remove double hamburger.

--- resources/js/script.js
+++ resources/js/script.js
@@ -97,58 +97,6 @@ $(document).ready(function() {
       offset: stickyOffset
   });
 
-  /* ============================= */  
-  /* ------For the slideshow------ */
-  /* ============================= */
-/*
-  // Initialize the slideshow at the first slide!
-  var slideIndex = 1;
-  showSlides(slideIndex);
-
-  // Next/previous controls
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-
-  // Thumbnail image controls
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
-
-  // The slideshow!
-  function showSlides(n) {
-  var i;
-  var slides = document.querySelectorAll('.slides');
-  var dots = document.querySelectorAll('.dot');
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = 'none'; 
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(' active', '');
-  }
-  slides[slideIndex-1].style.display = 'block'; 
-  dots[slideIndex-1].className += ' active';
-  }
-
-  // Slideshow controls
-  $('.prev').click(function () {
-    plusSlides(-1)
-  });
-  $('.next').click(function () {
-    plusSlides(1)
-  });
-  $('.dot1').click(function () {
-    currentSlide(1)
-  });
-  $('.dot2').click(function () {
-    currentSlide(2)
-  });
-  $('.dot3').click(function () {
-    currentSlide(3)
-  });
-*/
   /* =============================== */  
   /* For the buttons that scroll you */
   /* =============================== */
@@ -221,8 +169,6 @@ $(document).ready(function() {
     }
   } 
 
-
-
   $('#grConfigImg').click(function () {
     triggerModal('grConfigImg');
   });
@@ -290,37 +236,68 @@ $(document).ready(function() {
   // });
   
   /* Mobile nav */
-  $('.js--nav-icon').click(function() {
+//   $('.js--nav-icon').click(function() {
+//     var nav = $('.main-nav');
+//     var icon = $('.js--nav-icon i');  
+    
+//     nav.slideToggle(200);
+//     if (icon.hasClass('ion-md-menu')) {
+//         icon.addClass('ion-md-close');
+//         icon.removeClass('ion-md-menu');
+//     } else {
+//         icon.addClass('ion-md-menu');
+//         icon.removeClass('ion-md-close');
+//     }
+// });
+
+  $('.js--nav-icon').click(function() { 
       var nav = $('.main-nav');
-      var icon = $('.js--nav-icon i');
+      var icon = $('#mobile-nav-icon-image');
+      console.log(icon.attr("name"));
       
       nav.slideToggle(200);
-      if (icon.hasClass('ion-md-menu')) {
-          icon.addClass('ion-md-close');
-          icon.removeClass('ion-md-menu');
+      if (icon.attr("name") == "menu") {
+        icon.attr("name","close");
       } else {
-          icon.addClass('ion-md-menu');
-          icon.removeClass('ion-md-close');
+          icon.attr("name","menu");
       }
   });
   
-  $('.js--main-nav a').click(function(){
-      var nav = $('.js--main-nav');
-      var icon = $('.js--nav-icon i');
+  // $('.js--main-nav a').click(function(){
+  //     var nav = $('.js--main-nav');
+  //     var icon = $('.js--nav-icon i');
       
-      if ($(window).width() < 768){
-          nav.slideToggle(200);
+  //     if ($(window).width() < 768){
+  //         nav.slideToggle(200);
 
-      }
+  //     }
       
-      if (icon.hasClass('ion-md-menu')) {
-          icon.addClass('ion-md-close');
-          icon.removeClass('ion-md-menu');
-      } else {
-          icon.addClass('ion-md-menu');
-          icon.removeClass('ion-md-close');
-      }
-  });
+  //     if (icon.hasClass('ion-md-menu')) {
+  //         icon.addClass('ion-md-close');
+  //         icon.removeClass('ion-md-menu');
+  //     } else {
+  //         icon.addClass('ion-md-menu');
+  //         icon.removeClass('ion-md-close');
+  //     }
+  // });
+
+//   $('.js--main-nav a').click(function(){
+//     var nav = $('.js--main-nav');
+//     var icon = $('.js--nav-icon i');
+    
+//     if ($(window).width() < 768){
+//         nav.slideToggle(200);
+
+//     }
+    
+//     if (icon.hasClass('ion-md-menu')) {
+//         icon.addClass('ion-md-close');
+//         icon.removeClass('ion-md-menu');
+//     } else {
+//         icon.addClass('ion-md-menu');
+//         icon.removeClass('ion-md-close');
+//     }
+// });
   
   $(window).resize(function(){
       var nav = $('.js--main-nav');
